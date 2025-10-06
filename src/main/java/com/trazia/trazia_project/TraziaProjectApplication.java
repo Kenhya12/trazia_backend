b76fde67890ea30995a4fbb96b2484ac7731abd8
Paula Apsé <89433554+Kenhya12@users.noreply.github.com>
2025-10-06 15:52:49 +0200

    first commit

---
+++ src/main/java/com/trazia/trazia_project/TraziaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.trazia.trazia_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraziaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TraziaProjectApplication.class, args);
+	}
+
+}
